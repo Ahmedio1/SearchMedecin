ee605b273bfa617582a64af5b7f856b58b034ff7
Ahmed <ahmed__daoud@outlook.fr>
2023-06-28 15:31:00 +0200

    Requete API

--- src/BD/Api.ts
+++ src/BD/Api.ts
@@ -1,10 +1,29 @@
 import axios from 'axios';
 import Physician  from '../Models/Physician';
-
 const link = 'https://hds-staging.toktokdoc.com/physicians?search='
 
+class BD {
+    Physician: Physician[] = [];
+    
 
- function getPhysicianList(search: string){
-    return axios.get<Physician[]>(link + search);
+constructor() {
+    this.Physician = [];
+}
+
+async getPhysician(search: string) {
+    try{
+    const response = await axios.get(link + search);
+    const data = response.data;
+    this.Physician = data;
+    return this.Physician;
+    }
+    catch(error){
+        console.log(error);
+    }
+    return [];
+
 
 }
+}
+
+export default BD;
