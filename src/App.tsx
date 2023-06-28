bb71ad21a4c7b20269160a8d1cc34ed1ff79fb58
Ahmed <ahmed__daoud@outlook.fr>
2023-06-28 17:18:33 +0200

    Add commentaires

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import BD from './BD/Api';
 import Physician from './Models/Physician';
-
-
+import { Table, TableHead, TableRow, TableCell, TableBody, Button, Chip, ListItemText, ListItem, List, Input, Alert, Dialog } from '@mui/material';
+import { text } from 'stream/consumers';
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState<Physician[]>([]);
-  const [physicianList, setPhysicianList] = useState<Physician[]>([]);
+  const [searchTerm, setSearchTerm] = useState(''); // Search term
+  const [searchResults, setSearchResults] = useState<Physician[]>([]); // Search results
+  const [physicianList, setPhysicianList] = useState<Physician[]>([]); // List of physician
+  const [specialteList, setSpecialteList] = useState<string[]>([]); // List of specialite
+  const [physicianListOriginal, setListPhysicianOriginal] = useState<Physician[]>([]); // List of physician Original
+  const [filledSpecialite, setFilledSpecialite] = useState<string>(''); // Specialite filled
 
- 
-  const inputChange = (event:ChangeEvent<HTMLInputElement>) => {
+  // permet de faire la recherche
+  const inputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
     performSearch(event.target.value);
   };
@@ -23,52 +26,97 @@ function App() {
     setSearchResults(physicians);
   };
 
+  // ajoute dans les listes les medecins et les specialites si et seulement si ils ne sont pas déjà présent
   const addList = (physician: Physician) => {
-    if (physicianList.find((p) => p.identifiantpp === physician.identifiantpp)) {
+    if (physicianListOriginal.find((p) => p.identifiantpp === physician.identifiantpp)) {
       return;
     }
-    console.log(physician);
     setPhysicianList([...physicianList, physician]);
+    setListPhysicianOriginal([...physicianListOriginal, physician]);
+
+    if (specialteList.find((s) => s === physician.libellesavoirfaire) || physician.libellesavoirfaire == null) {
+      return;
+    }
+    setSpecialteList([...specialteList, physician.libellesavoirfaire]);
+
   };
 
+  // supprime un medecin de la liste
+  // supprime une spécialité des chips si aucun médecin n'a cette spécialité
   const removeList = (physician: Physician) => {
-    setPhysicianList(physicianList.filter((p) => p.identifiantpp !== physician.identifiantpp));
+    const confirmed = window.confirm('Voulez-vous supprimer ce médecin ?');
+    if (confirmed) {
+      setPhysicianList(physicianList.filter((p) => p.identifiantpp !== physician.identifiantpp));
+      if (physicianList.find((p) => p.libellesavoirfaire === physician.libellesavoirfaire && p.identifiantpp !== physician.identifiantpp)) {
+        return;
+      }
+      setSpecialteList(specialteList.filter((s) => s !== physician.libellesavoirfaire));
+    };
   };
+
+  // permet de trier les medecins par specialite
+  const trierSpecialite = (specialite: string) => {
+    if(specialite === filledSpecialite){
+      setFilledSpecialite('');
+      setPhysicianList(physicianListOriginal);
+      return;
+    }
+    const filteredList = physicianListOriginal.filter((p) => p.libellesavoirfaire === specialite);
+    setPhysicianList(filteredList);
+
+    setFilledSpecialite(specialite);
+  };
+
 
   return (
     <div className="App">
-      <input type="text"
+      <Input
+        type="text"
         value={searchTerm}
         onChange={inputChange}
-        placeholder="Recherche" ></input>
-        <ul>
-        {searchResults.map((physician,index) => (
-          <li onClick={() => addList(physician)} key={index}>{physician.nomdexercice}</li>
+        placeholder="Recherche"
+      />
+      <List>
+        {searchResults.map((physician, index) => (
+          <ListItemText primary={physician.nomdexercice} onClick={() => addList(physician)} key={index} />
         ))}
-        </ul>
-
-        <table>
-          <thead>
-            <tr>
-              <th>Nom</th>
-              <th>Prénom</th>
-              <th>Numero RPPS</th>
-              <th>Spécialité Medecin</th>
-            </tr>
-          </thead>
-          <tbody>
-            {physicianList.map((physician,index) => (
-              <tr key={index}>
-                <td>{physician.nomdexercice}</td>
-                <td>{physician.prenomdexercice}</td>
-                <td>{physician.identifiantpp}</td>
-                <td>{physician.libellesavoirfaire}</td>
-                <td><button onClick={() => removeList(physician)}>Supprimer</button></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      </List>
+      {specialteList.map((specialite) => (
+        <Chip
+          onClick={() => trierSpecialite(specialite)}
+          label={specialite}
+          variant={filledSpecialite === specialite ? 'filled' : 'outlined'}
+          color="secondary"
+          key={specialite}
+        />
+      ))}
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Nom</TableCell>
+            <TableCell>Prénom</TableCell>
+            <TableCell>Numero RPPS</TableCell>
+            <TableCell>Spécialité Médecin</TableCell>
+            <TableCell>Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {physicianList.map((physician, index) => (
+            <TableRow key={index}>
+              <TableCell>{physician.nomdexercice}</TableCell>
+              <TableCell>{physician.prenomdexercice}</TableCell>
+              <TableCell>{physician.identifiantpp}</TableCell>
+              <TableCell>{physician.libellesavoirfaire}</TableCell>
+              <TableCell>
+                <Button onClick={() => {
+                  removeList(physician);
+                }}>Supprimer</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
 
